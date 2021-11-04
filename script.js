b5a0d81a74f934d3a1e4f8f5b4bb0b2d980e08d1
aqsakhan <63785428+aqsakhan@users.noreply.github.com>
2021-11-04 16:44:36 +0530

    All files added

---
+++ script.js
@@ -0,0 +1,85 @@
+let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Show new quote
+function newQuote() {
+    showLoadingSpinner();
+    // Pick a random quote from the api
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // check quote length for styling
+    if(quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    // if the above cases dont follow
+    quoteText.textContent = quote.text;
+    // check if author field is blank
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Set quote hide loader
+    removeLoadingSpinner();
+}
+
+// Get quotes Frome API
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        alert(error);
+        // alert(error) or
+        // Catch Error here
+    }
+}
+
+// Tweet a quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On load
+getQuotes();
+
+
+
+
+
+
+// If not using the api use file "quotes.js"
+// function newQuote() {
+//     // Pick a random quote from the api
+//     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    // console.log(quote);
+// }
+// newQuote();
+
+// Important links:
+// https://quotes-react.netlify.app/
+// https://type.fit/api/quotes
